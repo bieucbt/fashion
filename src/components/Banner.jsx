31b70b1cb576eaa343a7ab3d528a9b38663283cc
bieucbt <bieucbt@gmail.com>
2025-01-01 17:04:29 +0700

    update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -15,18 +15,18 @@ const Banner = () => {
   };
 
   return (
-    <section className=''>
+    <section className='mt-[150px]'>
       <div className=''>
         <Slider {...settings}>
           {
             bannerData.map((item, i) => <div key={i} >
-            <div className='flex items-center h-[80vh]'>
+            <div className='flex items-center sm:h-[60vh] md:h-[75vh]'>
               <div className='flex-1'>
                 <p className='text-[14px] md:text-[16px] lg:text-[18px] text-blackText font-normal'>{item.subtext}</p>
                 <h3 className='text-[24px] md:text-[42px] lg:text-[80px] font-semibold text-blackText mt-5 '>{item.heading}</h3>
                 <div className='py-3 px-2 text-[14px] md:text-[16px] lg:text-[18px] md:text md:px-6 md:py-3 lg:px-10 lg:py-4 lg:font-semibold bg-blackText text-white w-max rounded-lg cursor-pointer mt-8'>Mua sắm ngay</div>
               </div>
-              <div className='flex-1 h-full'>
+              <div className='flex-1 h-full flex items-center'>
                 <img src={item.img} alt="quần áo mùa đông" className='w-full object-cover' />
               </div>
             </div>
