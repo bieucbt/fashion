4e7f936303f63d121828775469dc7272b9f70a62
bieucbt <bieucbt@gmail.com>
2025-01-31 20:17:37 +0700

    test

--- src/context/TokenProvider.jsx
+++ src/context/TokenProvider.jsx
@@ -2,10 +2,11 @@ import React, { createContext, useState } from 'react'
 
 export const TokenContext = createContext(null)
 
-const TokenProvider = ({children}) => {
+const TokenProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem('token') || '')
+  console.log(token)
   return (
-    <TokenContext.Provider value={{token, setToken}}>
+    <TokenContext.Provider value={{ token, setToken }}>
       {children}
     </TokenContext.Provider>
   )
