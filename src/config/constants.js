cf05fde5a19a61d55b1ed454fa700845d62dfd85
bieucbt <bieucbt@gmail.com>
2025-02-01 19:48:43 +0700

    test

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,7 +1,7 @@
 
 
-const BASE_URL = 'https://servertest-mmh4.onrender.com/user';
-const Admin_URL = 'https://admin-fashion.vercel.app'
+const BASE_URL = 'https://servertest-mmh4.onrender.com/user/';
+const Admin_URL = 'https://adminfashion.onrender.com'
 
 // const BASE_URL = 'http://localhost:3000/user/';
 // const Admin_URL = 'http://localhost:5174/'
