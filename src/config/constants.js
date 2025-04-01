8905a6c52d789c71d8b974321add93be274561cf
bieucbt <bieucbt@gmail.com>
2025-04-01 18:04:34 +0700

    update file constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,14 +1,14 @@
 
 
-// const USER_URL = 'https://servertest-mmh4.onrender.com/user/';
-// const PRODUCT_URL = 'https://servertest-mmh4.onrender.com/product/'
-// const Admin_URL = 'https://adminfashion.onrender.com'
-// const HOME_fASHION = 'https://fashion-ewea.onrender.com/'
+const USER_URL = 'https://servertest-mmh4.onrender.com/user/';
+const PRODUCT_URL = 'https://servertest-mmh4.onrender.com/product/'
+const Admin_URL = 'https://adminfashion.onrender.com'
+const HOME_fASHION = 'https://fashion-ewea.onrender.com/'
 
-const USER_URL = 'http://localhost:3000/user/';
-const PRODUCT_URL = 'http://localhost:3000/product/';
-const Admin_URL = 'http://localhost:5174/'
-const HOME_fASHION = 'http://localhost:5173/'
+// const USER_URL = 'http://localhost:3000/user/';
+// const PRODUCT_URL = 'http://localhost:3000/product/';
+// const Admin_URL = 'http://localhost:5174/'
+// const HOME_fASHION = 'http://localhost:5173/'
 
 const CLOUDINARY_URL = 'https://res.cloudinary.com/ddttnwsqh/image/upload/v1737878119/'
 
